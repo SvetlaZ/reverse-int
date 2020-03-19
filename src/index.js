93817be6a2bfc9d7e7bfeb6494bd8fa16089f983
Svetlana <svezan@mail.ru>
2020-03-19 13:00:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    n = Math.abs(n);
+    let m = '';
+    let count = String(n).length;
+    for (let i = 0; i < count; i++) {
+        m += n % 10;
+        n = Math.floor(n / 10); 
+    }
+      return m;
+  }
